6bec6a9b65d62ee5ac69f6a2f9f9ee252be767b1
houhengxing-hue <houhengxing@gmail.com>
2026-02-22 10:54:11 +0800

    Fix base for sturdy-barnacle

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sturdy-barnacle/',  // 若仓库名为 test；若为 username.github.io 则改为 '/'
+  base: '/sturdy-barnacle/',
 })
